e6543f583223c073bf64cd70ecf14159451f8f8d
Nikola <41384642+NT996@users.noreply.github.com>
2018-07-21 10:31:01 +0200

    Model (class)

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Producer { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Supplier { get; set; }
+        public decimal Price { get; set; }
+    }
+}
