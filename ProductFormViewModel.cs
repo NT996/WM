fb0acef5da8385a28a21b070ae95e2683563b641
Nikola <41384642+NT996@users.noreply.github.com>
2018-07-21 10:31:19 +0200

    View Model

---
+++ ProductFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project.Models;
+
+namespace Project.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public Product Product { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Product != null && Product.ID != 0)
+                    return "Edit Product";
+                return "New Product";
+            }
+        }
+    }
+}
